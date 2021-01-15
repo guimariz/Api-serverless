9eb1d9e837c6db4f976dbb7dafe341a64799e59e
guimariz <guimariz@gmail.com>
2021-01-15 20:01:13 -0300

    Configurando CreateProducts

--- CreateProduct/index.js
+++ CreateProduct/index.js
@@ -1,13 +1,16 @@
+const createMongoClient = require('../shared/mongoClient');
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const product = req.body;
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+  const Products = MongoClient.collection('products');
+  const res = await Products.insert(product);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  closeConnectionFn();
+
+  context.res = {
+    status: 201,
+    body: res,
+  };
+};
